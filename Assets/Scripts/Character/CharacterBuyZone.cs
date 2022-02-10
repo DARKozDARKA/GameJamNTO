b54314aceafbd3021b62c266c818c888de8187f5
Golub <golubnelox@yandex.ru>
2022-02-10 17:39:57 +0700

    ItemPickup

---
+++ Assets/Scripts/Character/CharacterBuyZone.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterBuyZone : MonoBehaviour
+{
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.layer == 9 && Input.GetKeyDown(KeyCode.F))
+        {
+            DoWithItem(other);
+        }
+    }
+
+    private void DoWithItem(Collider other)
+    {
+        print(other.gameObject.name);
+        Destroy(other.gameObject);
+    }
+}
