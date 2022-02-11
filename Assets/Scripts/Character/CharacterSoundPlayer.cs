a436ed6994370b783de4caa02745475550fb33e6
DARKozDARKA <kalinkovgoha04@gmail.com>
2022-02-11 16:00:24 +0300

    Sounds

---
+++ Assets/Scripts/Character/CharacterSoundPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSoundPlayer : MonoBehaviour
+{
+    [SerializeField] private AudioSource _source;
+    [SerializeField] private AudioClip[] _steps;
+    [SerializeField] private AudioClip[] _getItem;
+    [SerializeField] private AudioClip[] _cashOut;
+
+    public void PlayOneStep()
+    {
+        _source.pitch = Random.Range(0.9f, 1.1f);
+        _source.PlayOneShot(_steps[Random.Range(0, _steps.Length)]);
+    }
+
+    public void PlayGetItem()
+    {
+        _source.pitch = Random.Range(0.9f, 1.1f);
+        _source.PlayOneShot(_getItem[Random.Range(0, _getItem.Length)]);
+    }
+    public void PlayCashOut()
+    {
+        _source.pitch = Random.Range(0.9f, 1.1f);
+        _source.PlayOneShot(_cashOut[Random.Range(0, _cashOut.Length)]);
+    }
+}
